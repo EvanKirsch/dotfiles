c4b47a7180c1540332e1d92bff8531cbedb62715
Evan Kirsch <kirsch.j.evan@gmail.com>
2026-02-24 13:01:43 -0600

    Enable java and javascript lsp

--- home/.config/doom/init.el
+++ home/.config/doom/init.el
@@ -142,8 +142,8 @@
        ;;idris             ; a language you can depend on
        ;;json              ; At least it ain't XML
        ;;janet             ; Fun fact: Janet is me!
-       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (java +lsp +format) ; the poster child for carpal tunnel syndrome
+       javascript          ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
