37cedbcaf2a47f28eba5925fbc3b8837d6fa5db2
Evan Kirsch <kirsch.j.evan@gmail.com>
2026-04-03 10:18:26 -0500

    Enable latex formater

--- home/.config/doom/init.el
+++ home/.config/doom/init.el
@@ -146,7 +146,7 @@
        javascript          ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       ;;latex             ; writing papers in Emacs has never been so fun
+       (latex +format)        ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        (lua +lsp +format)  ; one-based indices? one-based indices
