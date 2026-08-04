293e095ee9677b2765ce8e5a66677ff6048d757e
Evan Kirsch <kirsch.j.evan@gmail.com>
2026-08-04 15:13:57 -0500

    Fix typo

--- symlinkConfigs.sh
+++ symlinkConfigs.sh
@@ -20,6 +20,6 @@ ln -s $SCRIPT_DIR/home/.config/doom ~/.config/doom
 # i3 configs
 ln -s $SCRIPT_DIR/home/.config/i3 ~/.config/i3
 
-i3 block impls
+# i3 block impls
 ln -s $SCRIPT_DIR/i3blocks/mediaplayer /usr/share/i3blocks/mediaplayer
 ln -s $SCRIPT_DIR/i3blocks/calendar /usr/share/i3blocks/calendar
